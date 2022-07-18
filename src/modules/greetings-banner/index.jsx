957cee81916c8ebbc0b4445d8ba119f6ef1ec3f6
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-19 02:27:38 +0700

    add modules greetings for auth page

---
+++ src/modules/greetings-banner/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
+
+import './index.scss';
+
+export default function GreetingsBanner(props) {
+  return (
+    <div className='container-greetings'>
+      <h1 className='greeting-title'>{props.title}</h1>
+      <div className='greeting-bar'></div>
+      <h4 className='greeting-description'>{props.description}</h4>
+      <div className='wrapper-greeting-not-have-account'>
+        <p>Don't have an account?</p>
+        <Link to='/signup' className='greeting-create-account'>
+          Create account
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+GreetingsBanner.proptypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
