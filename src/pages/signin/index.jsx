c787da18e9d359b8b3d27f14d06501ac785ef700
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-19 02:28:36 +0700

    implement signin page

--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import Input from "../../components/input";
-import "./index.scss";
+import { FormSignIn, GreetingsBanner } from '@modules';
+import './index.scss';
 
 export default function SignInPage() {
   return (
-    <div className='auth-container'>
-      <div>test</div>
-      <div>test</div>
-    </div>
+    <>
+      <div className='signin-form-container'>
+        <FormSignIn />
+      </div>
+      <div className='signin-greetings-container'>
+        <GreetingsBanner
+          title={`Welcome Back \nto\n Market Project`}
+          description={`Login to keep connected with us`}
+        />
+      </div>
+    </>
   );
 }
