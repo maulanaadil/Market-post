2f2457d4d2d34683951afbce11138e1bafb3bf73
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-19 17:02:52 +0700

    add draft, engagement pages

---
+++ src/pages/draft/index.jsx
@@ -0,0 +1,9 @@
+import Sidebar from '@modules/sidebar';
+
+export default function DraftPage() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
---
+++ src/pages/engagement/index.jsx
@@ -0,0 +1,9 @@
+import Sidebar from '@modules/sidebar';
+
+export default function EngagementPage() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
