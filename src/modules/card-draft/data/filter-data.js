69791999669c06bccf928474b0656568db3408e3
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-07-21 15:07:48 +0700

    filter dropdown

---
+++ src/modules/card-draft/data/filter-data.js
@@ -0,0 +1,12 @@
+const filterData = [
+  {
+    value: "lmn",
+    label: "Last Modified Newest",
+  },
+  {
+    value: "lmo",
+    label: "Last Modified Oldest",
+  },
+]
+
+export default filterData;
