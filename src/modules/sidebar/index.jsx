7270e1c10c62a14572b7e5b342ad8a5d89e2116d
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-07-22 09:16:17 +0700

    implement main-layout

--- src/modules/sidebar/index.jsx
+++ src/modules/sidebar/index.jsx
@@ -8,11 +8,11 @@ import './index.scss';
 
 export default function Sidebar() {
   return (
-    <div className='container-sidebar'>
-      <div className='wrapper-sidebar'>
-        <img src={Logo} alt='Market Project Logo' className='logo-sidebar' />
-        <div className='divider-sidebar'></div>
-        <div className='menu-sidebar'>
+    <div className="sidebar">
+      <div className="wrapper-sidebar">
+        <img src={Logo} alt="Market Project Logo" className="logo-sidebar" />
+        <div className="divider-sidebar"></div>
+        <div className="menu-sidebar">
           {navLinks.map((item) => (
             <MenuItem link={item.path} icon={item.icon} label={item.label} />
           ))}
