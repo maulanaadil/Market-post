de39db2378b26a5828e95e69616acd7ecec007b5
Risha Amara <87788235+amarsh30@users.noreply.github.com>
2022-07-22 15:19:35 +0700

    add Header (#7)
    * add Header
    
    * Setup Github Action (CI) (#8)
    
    * Create node.js.yml
    
    * add workflow ci
    
    Co-authored-by: Maulana Adil <maulanaadil.latif@gmail.com>
    
    * Implement CI on project (#9)
    
    * Create node.js.yml
    
    * add workflow ci
    
    * fix workflow ci
    
    * Create node.js.yml
    
    * add workflow ci
    
    * fix workflow ci
    
    * rename ci
    
    Co-authored-by: Maulana Adil <maulanaadil.latif@gmail.com>
    
    * Fix Setup CI (#10)
    
    * Create node.js.yml
    
    * add workflow ci
    
    * Create node.js.yml
    
    * add workflow ci
    
    * fix workflow ci
    
    * fix workflow ci
    
    Co-authored-by: Maulana Adil <maulanaadil.latif@gmail.com>
    
    * Create development-ci.yml
    
    * delete unused ci (#11)
    
    Co-authored-by: Maulana Adil <maulanaadil.latif@gmail.com>
    
    * Delete unused file (#12)
    
    * delete unused ci
    
    * rename ci
    
    Co-authored-by: Maulana Adil <maulanaadil.latif@gmail.com>
    
    * implement main-layout (#14)
    
    * card draft (#13)
    
    * publish feature/card-draft branch
    
    * update draft card
    
    * add draft edit and delete button
    
    * filter dropdown
    
    * update draft
    
    * card draft done
    
    * clean import
    
    * implement variable style
    
    * add Header
    
    * update header
    
    Co-authored-by: Maulana Adil <59109741+maulanaadil@users.noreply.github.com>
    Co-authored-by: Maulana Adil <maulanaadil.latif@gmail.com>
    Co-authored-by: IchsanN <72180474+ichsnn@users.noreply.github.com>

--- src/components/icon/index.jsx
+++ src/components/icon/index.jsx
@@ -6,11 +6,14 @@ import ConfigIcon from "@assets/icons/config.svg";
 import HomeIcon from "@assets/icons/home.svg";
 import EngagementIcon from "@assets/icons/engagement.svg";
 import DraftIcon from "@assets/icons/draft.svg";
+
 import DraftFilterDropdown from "@assets/icons/draft-filter-dropdown.svg";
 import DraftFilter from "@assets/icons/draft-filter.svg";
 import DraftCreate from "@assets/icons/draft-create.svg";
 import DraftEdit from   "@assets/icons/draft-edit.svg";
 import DraftDelete from "@assets/icons/draft-delete.svg";
+import NotifIcon from "@assets/icons/notif.svg";
+import HeaderDropdownIcon from "@assets/icons/header_dropdown.svg";
 
 export const iconTypes = {
   facebook: "FACEBOOK_ICON",
@@ -18,11 +21,15 @@ export const iconTypes = {
   home: "HOME_ICON",
   engagement: "ENGAGEMENT_ICON",
   draft: "DRAFT_ICON",
+
   draft_filter_dropdown: "DRAFT_FILTER_DROPDOWN",
   draft_filter: "DRAFT_FILTER",
   draft_create: "DRAFT_CREATE",
   draft_edit: "DRAFT_EDIT",
   draft_delete: "DRAFT_DELETE",
+  notif: "NOTIF_ICON",
+  header_dropdown: "HEADER_DROPDOWN_ICON",
+
 };
 
 const iconSrc = {
@@ -31,11 +38,17 @@ const iconSrc = {
   HOME_ICON: HomeIcon,
   ENGAGEMENT_ICON: EngagementIcon,
   DRAFT_ICON: DraftIcon,
+
+
   DRAFT_FILTER_DROPDOWN: DraftFilterDropdown,
   DRAFT_FILTER: DraftFilter,
   DRAFT_CREATE: DraftCreate,
   DRAFT_EDIT: DraftEdit,
-  DRAFT_DELETE: DraftDelete
+  DRAFT_DELETE: DraftDelete,
+
+  NOTIF_ICON: NotifIcon,
+  HEADER_DROPDOWN_ICON: HeaderDropdownIcon,
+
 };
 
 export default function Icon({ type }) {
